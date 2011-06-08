a562271ad7aa872f1d46597ebc7667d4fd988a39
Ren Shao <renshao@gmail.com>
2011-06-08 23:25:28 +1000

    1. added initial implementation. 2. added dependencies to commons-lang and jyaml.

--- src/main/java/javafaker/Faker.java
+++ src/main/java/javafaker/Faker.java
@@ -1,5 +1,62 @@
 package javafaker;
 
-public class Faker {
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
 
+import org.apache.commons.lang.math.RandomUtils;
+import org.ho.yaml.Yaml;
+
+public class Faker {
+    public static final Locale LOCALE = Locale.ENGLISH;
+
+    private final Map<String, Object> fakeValuesMap;
+
+    @SuppressWarnings({ "unchecked", "rawtypes" })
+    public Faker() {
+        String languageCode = LOCALE.getLanguage();
+        Map valuesMap = (Map) Yaml.load(ClassLoader.getSystemResourceAsStream(languageCode + ".yml"));
+        valuesMap = (Map) valuesMap.get(languageCode);
+        fakeValuesMap = (Map<String, Object>) valuesMap.get("faker");
+    }
+
+    public String firstName() {
+        return fetch("name.first_name");
+    }
+
+    public String lastName() {
+        return fetch("name.last_name");
+    }
+
+    public String prefix() {
+        return fetch("name.prefix");
+    }
+
+    public String phoneNumber() {
+        return numerify(fetch("phone_number.formats"));
+    }
+
+    public String numerify(String numberString) {
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < numberString.length(); i++) {
+            if (numberString.charAt(i) == '#') {
+                sb.append(RandomUtils.nextInt(10));
+            } else {
+                sb.append(numberString.charAt(i));
+            }
+        }
+
+        return sb.toString();
+    }
+
+    @SuppressWarnings("unchecked")
+    public String fetch(String key) {
+        String[] path = key.split("\\.");
+        Object currentValue = fakeValuesMap;
+        for (String pathSection : path) {
+            currentValue = ((Map<String, Object>) currentValue).get(pathSection);
+        }
+        List<String> valuesArray = (List<String>) currentValue;
+        return valuesArray.get(RandomUtils.nextInt(valuesArray.size()));
+    }
 }
--- src/test/java/javafaker/FakerTest.java
+++ src/test/java/javafaker/FakerTest.java
@@ -1,5 +1,19 @@
 package javafaker;
 
-public class FakerTest {
+import org.junit.Assert;
+import org.junit.Test;
 
+public class FakerTest {
+    @Test
+    public void shouldFetchCorrectValue() {
+        Faker faker = new Faker();
+        String firstName = faker.firstName();
+        Assert.assertNotNull(firstName);
+        String lastName = faker.lastName();
+        Assert.assertNotNull(lastName);
+        String prefix = faker.prefix();
+        Assert.assertNotNull(prefix);
+        String phoneNumber = faker.phoneNumber();
+        Assert.assertNotNull(phoneNumber);
+    }
 }
