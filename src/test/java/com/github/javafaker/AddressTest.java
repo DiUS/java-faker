8fe2a69eb3260f71011d2cdddc41c9a5cac98117
Ricky <ricky@dius.com.au>
2014-04-24 21:39:34 +1000

    refactored tests to make all the boring tests in the IntegrationTest class

--- src/test/java/com/github/javafaker/AddressTest.java
+++ src/test/java/com/github/javafaker/AddressTest.java
@@ -1,120 +1,21 @@
 package com.github.javafaker;
 
+import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-
-import java.util.Locale;
-import java.util.Random;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import static com.github.javafaker.matchers.IsANumber.isANumber;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.junit.Assert.assertNotNull;
 
-@RunWith(value = Parameterized.class)
-public class AddressTest extends AbstractFakerTest {
+public class AddressTest {
 
-    public AddressTest(Locale locale, Random random) {
-        super(locale, random);
-    }
+    private static final Logger logger = LoggerFactory.getLogger(AddressTest.class);
+    private Faker faker;
 
-    @Test
-    public void testStreetName() {
-        String streetName = faker.streetName();
-        logger.info("Street name: " + streetName);
-        assertNotNull(streetName);
-
-        streetName = faker.address().streetName();
-        logger.info("Street name: " + streetName);
-        assertNotNull(streetName);
-    }
-
-    @Test
-    public void testStreetAddress() {
-        String streetAddress = faker.streetAddress(true);
-        logger.info("Street address: " + streetAddress);
-        assertNotNull(streetAddress);
-
-        streetAddress = faker.address().streetAddress(true);
-        logger.info("Street address: " + streetAddress);
-        assertNotNull(streetAddress);
-    }
-
-    @Test
-    public void testSecondaryAddress() {
-        String secondaryAddress = faker.secondaryAddress();
-        logger.info("Secondary address: " + secondaryAddress);
-        assertNotNull(secondaryAddress);
-
-        secondaryAddress = faker.address().secondaryAddress();
-        logger.info("Secondary address: " + secondaryAddress);
-        assertNotNull(secondaryAddress);
-    }
-
-    @Test
-    public void testZipCode() {
-        String zip = faker.zipCode();
-        logger.info("Address zip code: " + zip);
-        assertNotNull(zip);
-
-        zip = faker.address().zipCode();
-        logger.info("Address zip code: " + zip);
-        assertNotNull(zip);
-    }
-
-    @Test
-    public void testStreetSuffix() {
-        String streetSuffix = faker.streetSuffix();
-        logger.info("Street suffix: " + streetSuffix);
-        assertNotNull(streetSuffix);
-
-        streetSuffix = faker.address().streetSuffix();
-        logger.info("Street suffix: " + streetSuffix);
-        assertNotNull(streetSuffix);
-    }
-
-    @Test
-    public void testCitySuffix() {
-        String citySuffix = faker.citySuffix();
-        logger.info("City suffix: " + citySuffix);
-        assertNotNull(citySuffix);
-
-        citySuffix = faker.address().citySuffix();
-        logger.info("City suffix: " + citySuffix);
-        assertNotNull(citySuffix);
-    }
-
-    @Test
-    public void testCityPrefix() {
-        String cityPrefix = faker.cityPrefix();
-        logger.info("City prefix: " + cityPrefix);
-        assertNotNull(cityPrefix);
-
-        cityPrefix = faker.address().cityPrefix();
-        logger.info("City prefix: " + cityPrefix);
-        assertNotNull(cityPrefix);
-    }
-
-    @Test
-    public void testStateAbbr() {
-        String stateAbbr = faker.stateAbbr();
-        logger.info("State abbr: " + stateAbbr);
-        assertNotNull(stateAbbr);
-
-        stateAbbr = faker.address().stateAbbr();
-        logger.info("State abbr: " + stateAbbr);
-        assertNotNull(stateAbbr);
-    }
-
-    @Test
-    public void testCountry() {
-        String country = faker.country();
-        logger.info("Country: " + country);
-        assertNotNull(country);
-
-        country = faker.address().country();
-        logger.info("Country: " + country);
-        assertNotNull(country);
+    @Before
+    public void before() {
+        faker = new Faker();
     }
 
     @Test
--- src/test/java/com/github/javafaker/LoremTest.java
+++ src/test/java/com/github/javafaker/LoremTest.java
@@ -1,86 +1,17 @@
 package com.github.javafaker;
 
-import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
-import java.util.List;
-import java.util.Locale;
-import java.util.Random;
-
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
 
-public class LoremTest extends AbstractFakerTest {
+public class LoremTest {
 
-    public LoremTest(Locale locale, Random random) {
-        super(locale, random);
-    }
+    private Faker faker;
 
-    @Test
-    public void testSpecifiedNumOfWords() {
-        List<String> words = faker.words(4);
-        logger.info("Test words: " + words.toString());
-        assertEquals(4, words.size());
-
-        words = faker.lorem().words(4);
-        logger.info("Test words: " + words.toString());
-        assertEquals(4, words.size());
-    }
-
-    @Test
-    public void testSentence() {
-        String sentence = faker.sentence(7);
-        logger.info("Test sentence: " + sentence);
-        assertNotNull(sentence);
-
-        sentence = faker.lorem().sentence(7);
-        logger.info("Test sentence: " + sentence);
-        assertNotNull(sentence);
-    }
-
-    @Test
-    public void testSentences() {
-        List<String> sentences = faker.sentences(3);
-        logger.info("Test sentences: " + sentences);
-        assertEquals(3, sentences.size());
-
-        sentences = faker.lorem().sentences(3);
-        logger.info("Test sentences: " + sentences);
-        assertEquals(3, sentences.size());
-    }
-
-    @Test
-    public void testSpecifiedNumOfParagraphs() {
-        String paragraph = faker.paragraph(5);
-        logger.info("Test paragraph: " + paragraph);
-        assertNotNull(paragraph);
-
-        paragraph = faker.lorem().paragraph(5);
-        logger.info("Test paragraph: " + paragraph);
-        assertNotNull(paragraph);
-    }
-
-    @Test
-    public void testParagraph() {
-        String paragraph = faker.paragraph();
-        logger.info("Paragraph: " + paragraph);
-        assertNotNull(paragraph);
-
-        paragraph = faker.lorem().paragraph();
-        logger.info("Paragraph: " + paragraph);
-        assertNotNull(paragraph);
-    }
-
-    @Test
-    public void testParagraphs() {
-        final int paragraphCount = 10;
-        List<String> paragraphs = faker.paragraphs(paragraphCount);
-        logger.info("Paragraphs: " + paragraphs);
-        assertEquals(paragraphCount, paragraphs.size());
-
-        paragraphs = faker.lorem().paragraphs(paragraphCount);
-        logger.info("Paragraphs: " + paragraphs);
-        assertEquals(paragraphCount, paragraphs.size());
+    @Before
+    public void before() {
+        faker = new Faker();
     }
 
     @Test
