03f60d080950aa760176adda3ec33452e8966758
Isaac Sadaqah <isaac@arrayiq.net>
2014-06-19 08:26:10 -0400

    Add unit tests for latitude and longitude

--- src/test/java/com/github/javafaker/AddressTest.java
+++ src/test/java/com/github/javafaker/AddressTest.java
@@ -7,6 +7,9 @@ import org.slf4j.LoggerFactory;
 
 import static com.github.javafaker.matchers.IsANumber.isANumber;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 public class AddressTest {
 
@@ -23,5 +26,31 @@ public class AddressTest {
         final String streetAddressNumber = faker.address().streetAddressNumber();
         logger.info("Street Address Number: " + streetAddressNumber);
         assertThat(streetAddressNumber, isANumber());
+    }
+
+    @Test
+    public void testLatitude() {
+        try {
+            String latStr = faker.address().latitude();
+            logger.info("Latitude: {}", latStr);
+            assertNotNull(latStr);
+            final Double lat = new Double(latStr);
+            assertTrue(lat > -90 && lat < 90);
+        } catch (NumberFormatException e) {
+            fail("Invalid Latitude");
+        }
+    }
+
+    @Test
+    public void testLongitude() {
+        try {
+            String longStr = faker.address().longitude();
+            logger.info("Longitude: {}", longStr);
+            assertNotNull(longStr);
+            final Double lat = new Double(longStr);
+            assertTrue(lat > -180 && lat < 180);
+        } catch (NumberFormatException e) {
+            fail("Invalid Longitude");
+        }
     }
 }
