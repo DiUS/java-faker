a31c4e345b34eec8c3b281a9dd34ce1126c38026
Pascal Schumacher <pascalschumacher@gmx.net>
2016-04-09 10:44:54 +0200

    add french and finish company names (fixes #58)

--- src/test/java/com/github/javafaker/integration/FakerIT.java
+++ src/test/java/com/github/javafaker/integration/FakerIT.java
@@ -66,6 +66,7 @@ public class FakerIT {
         testAllMethodsThatReturnStringsActuallyReturnStrings(faker);
         testAllMethodsThatReturnStringsActuallyReturnStrings(faker.address());
         testAllMethodsThatReturnStringsActuallyReturnStrings(faker.business());
+        testAllMethodsThatReturnStringsActuallyReturnStrings(faker.company());
         testAllMethodsThatReturnStringsActuallyReturnStrings(faker.internet());
         testAllMethodsThatReturnStringsActuallyReturnStrings(faker.lorem());
         testAllMethodsThatReturnStringsActuallyReturnStrings(faker.phoneNumber());
