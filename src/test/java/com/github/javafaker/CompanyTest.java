3956349ecdb7a38ef4dec745e1485533afa48df1
Pascal Schumacher <pascalschumacher@gmx.net>
2016-04-14 19:14:23 +0200

    add unit test for company

---
+++ src/test/java/com/github/javafaker/CompanyTest.java
@@ -0,0 +1,27 @@
+package com.github.javafaker;
+
+import static com.github.javafaker.matchers.MatchesRegularExpression.matchesRegularExpression;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class CompanyTest {
+
+    private Faker faker;
+
+    @Before
+    public void before() {
+        faker = new Faker();
+    }
+
+    @Test
+    public void testName() {
+        assertThat(faker.company().name(), matchesRegularExpression("[A-Za-z\\-&', ]+"));
+    }
+
+    @Test
+    public void testSuffix() {
+        assertThat(faker.company().suffix(), matchesRegularExpression("[A-Za-z ]+"));
+    }
+}
