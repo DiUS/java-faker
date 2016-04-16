f756bb36ae15ae5feaacceb39421f87835771743
Christophe Willemsen <willemsen.christophe@gmail.com>
2016-04-16 03:46:06 +0200

    tests for numberBetween

--- src/test/java/com/github/javafaker/NumberTest.java
+++ src/test/java/com/github/javafaker/NumberTest.java
@@ -3,6 +3,8 @@ package com.github.javafaker;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.lang.*;
+import java.lang.Number;
 import java.math.BigDecimal;
 
 import static org.junit.Assert.*;
@@ -69,5 +71,22 @@ public class NumberTest {
                 }
             }
         }
+    }
+
+    @Test
+    public void testNumberBetween() {
+        for (int i = 1; i < 100; ++i) {
+            int v = faker.number().numberBetween(0, i);
+            assertTrue(v <= i && v >= 0);
+        }
+
+        for (long i = 1L; i < 100L; ++i) {
+            long v = faker.number().numberBetween(0, i);
+            assertTrue(v <= i && v >= 0);
+        }
+
+        int min1 = 1;
+        long v1 = faker.number().numberBetween(min1, 980000000L);
+        assertTrue(v1 > min1 && v1 < 980000000L);
     }
 }
