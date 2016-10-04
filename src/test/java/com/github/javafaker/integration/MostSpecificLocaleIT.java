690415f159e8b5c7dc6f673ad220f86620a5cea8
Trever Shick <trevershick@gmail.com>
2016-10-03 23:53:28 -0400

    Handle legacy-style constructed Locales (#128)
    Closes DiUS/java-faker#125
    
    Fixes a couple of issues.
    1) when constructing a new locale like new Locale("pt-br"), the correct file /pt-BR.yml will be loaded.
    2) the correct yaml file is returned by language and country code.  loading a new Faker with en_US would return /en.yml and not /en-US.yml

---
+++ src/test/java/com/github/javafaker/integration/MostSpecificLocaleIT.java
@@ -0,0 +1,40 @@
+package com.github.javafaker.integration;
+
+import com.github.javafaker.service.FakeValuesService;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+import java.util.Locale;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.assertThat;
+
+/**
+ * The purpose of these tests is to ensure that the Locales have been properly configured
+ * and that methods return values. The unit tests should ensure what the values returned
+ * are correct. These tests just ensure that the methods can be invoked.
+ */
+public class MostSpecificLocaleIT {
+
+    private FakeValuesService en;
+    private FakeValuesService en_US;
+
+    @Before
+    public void setupFakers() {
+        en = new FakeValuesService(new Locale("en"), null);
+        en_US = new FakeValuesService(new Locale("en","US"), null);
+    }
+
+    @Test
+    public void resolvesTheMostSpecificLocale() {
+        
+        final List<String> enDefaultCountries = (List<String>) en.fetchObject("address.default_country");
+        final List<String> enUsDefaultCountries = (List<String>) en_US.fetchObject("address.default_country");
+        
+        assertThat(enDefaultCountries, hasSize(1));
+        assertThat(enUsDefaultCountries, hasSize(3));
+        
+        assertThat("the default country for en is not en_US", enDefaultCountries, is(not(enUsDefaultCountries)));
+    }
+}
