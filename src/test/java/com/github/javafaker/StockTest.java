81bd0e71e47a1dc06969dee308515e9a81bc2108
Noel OConnor <noel.oconnor@gmail.com>
2016-11-30 19:16:28 +0000

    updated unit test to use matcher and removed unused imports

--- src/test/java/com/github/javafaker/StockTest.java
+++ src/test/java/com/github/javafaker/StockTest.java
@@ -1,25 +1,22 @@
 package com.github.javafaker;
 
-import static org.junit.Assert.*;
-
-import org.junit.Test;
-
-import org.junit.Test;
-
-import static com.github.javafaker.matchers.MatchesRegularExpression.matchesRegularExpression;
+import static org.hamcrest.Matchers.isEmptyOrNullString;
+import static org.hamcrest.Matchers.not;
 import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
 
 public class StockTest extends AbstractFakerTest {
 
 
 	@Test
 	public void testNasdaq() {
-		assert(faker.stock().nsdqSymbol() != "");
+		assertThat(faker.stock().nsdqSymbol(), not(isEmptyOrNullString()));
 	}
 	
 	@Test
 	public void testNYSE() {
-		assert(faker.stock().nyseSymbol() != "");
+		assertThat(faker.stock().nyseSymbol(), not(isEmptyOrNullString()));
 	}
 
 }
