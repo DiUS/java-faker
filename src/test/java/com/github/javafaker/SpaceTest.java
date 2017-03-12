061d3a5ba0d0216e0e2824e19d23566efceac527
Pascal Schumacher <pascalschumacher@gmx.net>
2017-03-12 22:32:29 +0100

    add Space#meteorite (#203)

--- src/test/java/com/github/javafaker/SpaceTest.java
+++ src/test/java/com/github/javafaker/SpaceTest.java
@@ -8,62 +8,67 @@ import org.junit.Test;
 public class SpaceTest extends AbstractFakerTest {
 
     @Test
-    public void testPlanet() {
+    public void planet() {
         assertThat(faker.space().planet(), matchesRegularExpression("(\\w+ ?){2,3}"));
     }
 
     @Test
-    public void testMoon() {
+    public void moon() {
         assertThat(faker.space().moon(), matchesRegularExpression("(\\w+ ?){2,3}"));
     }
 
     @Test
-    public void testGalaxy() {
+    public void galaxy() {
         assertThat(faker.space().galaxy(), matchesRegularExpression("(\\w+ ?){2,3}"));
     }
 
     @Test
-    public void testNebula() {
+    public void nebula() {
         assertThat(faker.space().nebula(), matchesRegularExpression("(\\w+ ?){2,3}"));
     }
 
     @Test
-    public void testStarCluster() {
+    public void starCluster() {
         assertThat(faker.space().starCluster(), matchesRegularExpression("(\\w+[ -]?){1,3}"));
     }
 
     @Test
-    public void testConstellation() {
+    public void constellation() {
         assertThat(faker.space().constellation(), matchesRegularExpression("(\\w+ ?){2,3}"));
     }
 
     @Test
-    public void testStar() {
+    public void star() {
         assertThat(faker.space().star(), matchesRegularExpression("(\\w+[ -]?){2,3}"));
     }
 
     @Test
-    public void testAgency() {
+    public void agency() {
         assertThat(faker.space().agency(), matchesRegularExpression("(\\w+ ?){2,5}"));
     }
 
     @Test
-    public void testAgencyAbbreviation() {
+    public void agencyAbbreviation() {
         assertThat(faker.space().agencyAbbreviation(), matchesRegularExpression("(\\w+ ?){2,3}"));
     }
 
     @Test
-    public void testNasaSpaceCraft() {
+    public void nasaSpaceCraft() {
         assertThat(faker.space().nasaSpaceCraft(), matchesRegularExpression("(\\w+ ?){2,3}"));
     }
 
     @Test
-    public void testCompany() {
+    public void company() {
         assertThat(faker.space().company(), matchesRegularExpression("(\\w+ ?){2,4}"));
     }
 
     @Test
-    public void testDistanceMeasurement() {
+    public void distanceMeasurement() {
         assertThat(faker.space().distanceMeasurement(), matchesRegularExpression("(\\w+ ?){2,3}"));
+    }
+
+    @Test
+    public void meteorite() {
+        assertThat(faker.space().meteorite(), matchesRegularExpression("(?U)([\\w()]+[ -–]?){1,4}"));
     }
 }
