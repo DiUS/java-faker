d3db25d4bd4072120b8107d779c4786a9c62a26f
Luka <robosoul@users.noreply.github.com>
2017-05-25 07:36:47 +0200

    Add TwinPeaks (#231)

---
+++ src/test/java/com/github/javafaker/TwinPeaksTest.java
@@ -0,0 +1,26 @@
+package com.github.javafaker;
+
+import org.junit.Test;
+
+import static com.github.javafaker.matchers.MatchesRegularExpression.matchesRegularExpression;
+import static org.hamcrest.Matchers.isEmptyOrNullString;
+import static org.hamcrest.core.IsNot.not;
+import static org.junit.Assert.assertThat;
+
+public class TwinPeaksTest extends AbstractFakerTest {
+
+    @Test
+    public void character() throws Exception {
+        assertThat(faker.twinPeaks().character(), matchesRegularExpression("^([\\w']+ ?){2,}$"));
+    }
+
+    @Test
+    public void location() throws Exception {
+        assertThat(faker.twinPeaks().location(), matchesRegularExpression("^[A-Za-z0-9'&,\\- ]+$"));
+    }
+
+    @Test
+    public void quote() throws Exception {
+        assertThat(faker.twinPeaks().quote(), not(isEmptyOrNullString()));
+    }
+}
