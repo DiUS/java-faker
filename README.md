18e85a14cf747565b7113d691f2a0b19cb70d06e
robosoul <nomorestories@gmail.com>
2017-05-25 10:32:22 +0200

    Add Yoda

--- README.md
+++ README.md
@@ -101,6 +101,7 @@ Fakers
 * Team
 * TwinPeaks
 * University
+* Yoda
 * Zelda
 
 Usage with Locales
--- src/test/java/com/github/javafaker/integration/FakerIT.java
+++ src/test/java/com/github/javafaker/integration/FakerIT.java
@@ -133,6 +133,7 @@ public class FakerIT {
         testAllMethodsThatReturnStringsActuallyReturnStrings(faker.hipster());
         testAllMethodsThatReturnStringsActuallyReturnStrings(faker.job());
         testAllMethodsThatReturnStringsActuallyReturnStrings(faker.twinPeaks());
+        testAllMethodsThatReturnStringsActuallyReturnStrings(faker.yoda());
     }
 
     private void testAllMethodsThatReturnStringsActuallyReturnStrings(Object object) throws Exception {
