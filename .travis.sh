f38c6af4ac5a7e0c6e0933a3ae48a5d8d1bc42b7
Bartłomiej Szymański <github.com@draakhan.info>
2017-09-09 20:48:29 +0200

    Missing keys added

---
+++ .travis.sh
@@ -0,0 +1,2 @@
+sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+sudo sh -c "echo deb https://apt.dockerproject.org/repo ubuntu-trusty main > /etc/apt/sources.list.d/docker.list"
