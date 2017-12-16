5a8020d3efc01ae9d275c8b176b64a91b46e249d
Taylor Wilson <twilson@instructure.com>
2017-12-16 16:30:32 -0700

    Add repeat annotation to avatar test

--- src/test/java/com/github/javafaker/AvatarTest.java
+++ src/test/java/com/github/javafaker/AvatarTest.java
@@ -1,5 +1,6 @@
 package com.github.javafaker;
 
+import com.github.javafaker.repeating.Repeat;
 import org.junit.Test;
 
 import static com.github.javafaker.matchers.MatchesRegularExpression.matchesRegularExpression;
@@ -8,6 +9,7 @@ import static org.junit.Assert.assertThat;
 public class AvatarTest extends AbstractFakerTest {
 
     @Test
+    @Repeat(times=10)
     public void testAvatar() {
         String avatar = faker.avatar().image();
         assertThat(avatar, matchesRegularExpression("^https:\\/\\/s3.amazonaws\\.com\\/uifaces\\/faces\\/twitter\\/[a-zA-Z0-9_]+\\/128\\.jpg$"));
