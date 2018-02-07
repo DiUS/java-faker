60e587b3b7c20e67129f762de1f781d3bc1edfbb
Serge Bishyr <seriybg@gmail.com>
2018-02-07 13:21:16 +0200

    Add generator for temperature in a range

--- src/main/java/com/github/javafaker/Weather.java
+++ src/main/java/com/github/javafaker/Weather.java
@@ -1,5 +1,8 @@
 package com.github.javafaker;
 
+/**
+ * A generator for Weather data.
+ */
 public class Weather {
 
     private static final int DEFAULT_MIN_TEMP_C = -30;
@@ -13,15 +16,50 @@ public class Weather {
         this.faker = faker;
     }
 
+    /**
+     * Generates a short weather description.
+     */
     public String description() {
         return faker.resolve("weather.description");
     }
 
+    /**
+     * Generates a random temperature celsius between -30 and 38 degrees.
+     * @return String that represents temperature in format 5°C
+     */
     public String temperatureCelsius() {
-        return faker.random().nextInt(DEFAULT_MIN_TEMP_C, DEFAULT_MAX_TEMP_C) + faker.resolve("weather.temperature.celsius");
+        return temperature(DEFAULT_MIN_TEMP_C, DEFAULT_MAX_TEMP_C, "weather.temperature.celsius");
     }
 
+    /**
+     * Generates a random temperature fahrenheit between -22 and 100 degrees.
+     * @return String that represents temperature in format 5°F
+     */
     public String temperatureFahrenheit() {
-        return faker.random().nextInt(DEFAULT_MIN_TEMP_F, DEFAULT_MAX_TEMP_F) + faker.resolve("weather.temperature.fahrenheit");
+        return temperature(DEFAULT_MIN_TEMP_F, DEFAULT_MAX_TEMP_F, "weather.temperature.fahrenheit");
+    }
+
+    /**
+     * Generates a random temperature celsius between two temperatures.
+     * @param minTemperature the minimal temperature
+     * @param maxTemperature the maximal temperature
+     * @return String that represents temperature in format 5°C
+     */
+    public String temperatureCelsius(int minTemperature, int maxTemperature) {
+        return temperature(minTemperature, maxTemperature, "weather.temperature.celsius");
+    }
+
+    /**
+     * Generates a random temperature fahrenheit between two temperatures.
+     * @param minTemperature the minimal temperature
+     * @param maxTemperature the maximal temperature
+     * @return String that represents temperature in format 5°F
+     */
+    public String temperatureFahrenheit(int minTemperature, int maxTemperature) {
+        return temperature(minTemperature, maxTemperature, "weather.temperature.fahrenheit");
+    }
+
+    private String temperature(int minTemperature, int maxTemperature, String degreeKey) {
+        return faker.random().nextInt(minTemperature, maxTemperature) + faker.resolve(degreeKey);
     }
 }
--- src/test/java/com/github/javafaker/WeatherTest.java
+++ src/test/java/com/github/javafaker/WeatherTest.java
@@ -23,4 +23,18 @@ public class WeatherTest extends AbstractFakerTest {
     public void temperatureFahrenheit() {
         assertThat(faker.weather().temperatureFahrenheit(), matchesRegularExpression("[-]?\\d+°F"));
     }
+
+    @Test
+    public void temperatureCelsiusInRange() {
+        for (int i = 1; i < 100; i++) {
+            assertThat(faker.weather().temperatureCelsius(-5, 5), matchesRegularExpression("[-]?[0-5]°C"));
+        }
+    }
+
+    @Test
+    public void temperatureFahrenheitInRange() {
+        for (int i = 1; i < 100; i++) {
+            assertThat(faker.weather().temperatureFahrenheit(-5, 5), matchesRegularExpression("[-]?[0-5]°F"));
+        }
+    }
 }
