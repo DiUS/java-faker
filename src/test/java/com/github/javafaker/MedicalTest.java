ea463fed2b9e892d122b1d57db3e3463f0a4b0a5
Vikram <vikram@waterlinedata.com>
2018-09-25 16:34:52 +0530

    Modified regular expression for Hospital Name

--- src/test/java/com/github/javafaker/MedicalTest.java
+++ src/test/java/com/github/javafaker/MedicalTest.java
@@ -9,16 +9,19 @@ public class MedicalTest extends AbstractFakerTest {
 
     @Test
     public void testMedicineName() {
-        assertThat(faker.medical().medicineName(), matchesRegularExpression("([\\w']+\\.?( )?){2,4}"));
+        assertThat(faker.medical().medicineName(), matchesRegularExpression("([\\w']+\\.?( )?){2,5}"));
     }
+
     @Test
     public void testDiseaseName() {
         assertThat(faker.medical().diseaseName(), matchesRegularExpression("([\\w']+\\.?( )?){2,4}"));
     }
+
     @Test
     public void testHospitalName() {
-        assertThat(faker.medical().hospitalName(), matchesRegularExpression("([\\w']+\\.?( )?){2,4}"));
+        assertThat(faker.medical().hospitalName(), matchesRegularExpression("([\\w']+\\.?( )?){2,8}"));
     }
+
     @Test
     public void testSymptom() {
         assertThat(faker.medical().symptoms(), matchesRegularExpression("([\\w']+\\.?.\\(?[\\w']\\)?( )?){2,4}"));
@@ -28,6 +31,11 @@ public class MedicalTest extends AbstractFakerTest {
     @Test
     public void testSymptomStatic() {
         assertThat("Infertility (Female)", matchesRegularExpression("([\\w']+\\.?.\\(?[\\w']\\)?( )?){2,4}"));
+    }
+
+    @Test
+    public void testHospitalStatic() {
+        assertThat("ST VINCENT SALEM HOSPITAL INC", matchesRegularExpression("([\\w']+\\.?.\\(?[\\w']\\)?( )?){2,5}"));
     }
 
 }
