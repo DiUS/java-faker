0098bd765602e32f45911ebb4300760070c72648
Vincent Poirier <poirier.20.100@hotmail.com>
2018-12-05 09:11:57 -0500

    Put test reference in loop to avoid imprecision

--- src/test/java/com/github/javafaker/DateAndTimeTest.java
+++ src/test/java/com/github/javafaker/DateAndTimeTest.java
@@ -31,25 +31,23 @@ public class DateAndTimeTest extends AbstractFakerTest {
 
     @Test
     public void testFutureDateWithMinimum(){
-        Date now = new Date();
-
         for (int i = 0; i < 1000; i++) {
+            Date now = new Date();
             Date future = faker.date().future(5, 4, TimeUnit.SECONDS);
             assertThat("past date", future.getTime(), greaterThan(now.getTime()));
-            assertThat("future date over range", future.getTime(), lessThan(now.getTime() + 5000));
-            assertThat("future date under minimum range", future.getTime(), greaterThan(now.getTime() + 4000));
+            assertThat("future date over range", future.getTime(), lessThan(now.getTime() + 5001));
+            assertThat("future date under minimum range", future.getTime(), greaterThan(now.getTime() + 3999));
         }
     }
 
     @Test
     public void testPastDateWithMinimum(){
-        Date now = new Date();
-
         for (int i = 0; i < 1000; i++) {
+            Date now = new Date();
             Date past = faker.date().past(5, 4, TimeUnit.SECONDS);
             assertThat("future date", past.getTime(), lessThan(now.getTime()));
-            assertThat("past date over range", past.getTime(), greaterThan(now.getTime() - 5000));
-            assertThat("past date under minimum range", past.getTime(), lessThan(now.getTime() - 4000));
+            assertThat("past date over range", past.getTime(), greaterThan(now.getTime() - 5001));
+            assertThat("past date under minimum range", past.getTime(), lessThan(now.getTime() - 3999));
         }
     }
 
