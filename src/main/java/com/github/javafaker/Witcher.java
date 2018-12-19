1fae9ce05e50537af3d15ea348c2949dec43d06a
Ricky Yim <ricky.yim@domain.com.au>
2018-12-19 16:08:31 +1100

    Made the FakeValuesService support splitting up locale yml files into multiple files

--- src/main/java/com/github/javafaker/Witcher.java
+++ src/main/java/com/github/javafaker/Witcher.java
@@ -8,26 +8,26 @@ public class Witcher {
     }
     
     public String character() {
-        return faker.fakeValuesService().resolve("witcher.characters", this, faker);
+        return faker.fakeValuesService().resolve("games.witcher.characters", this, faker);
     }
     
     public String witcher() {
-        return faker.fakeValuesService().resolve("witcher.witchers", this, faker);
+        return faker.fakeValuesService().resolve("games.witcher.witchers", this, faker);
     }
     
     public String school() {
-        return faker.fakeValuesService().resolve("witcher.schools", this, faker);
+        return faker.fakeValuesService().resolve("games.witcher.schools", this, faker);
     }
     
     public String location() {
-        return faker.fakeValuesService().resolve("witcher.locations", this, faker);
+        return faker.fakeValuesService().resolve("games.witcher.locations", this, faker);
     }
     
     public String quote() {
-        return faker.fakeValuesService().resolve("witcher.quotes", this, faker);
+        return faker.fakeValuesService().resolve("games.witcher.quotes", this, faker);
     }
     
     public String monster() {
-        return faker.fakeValuesService().resolve("witcher.monsters", this, faker);
+        return faker.fakeValuesService().resolve("games.witcher.monsters", this, faker);
     }
 }
