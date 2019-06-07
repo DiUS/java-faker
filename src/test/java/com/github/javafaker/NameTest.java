e91e7f9a3bab36497a864422baad7217adfbb4b4
SS007979 <Shaule.Sachs@penfed.org>
2019-06-07 12:03:56 -0400

    Adding Assert.assertEquals import

--- src/test/java/com/github/javafaker/NameTest.java
+++ src/test/java/com/github/javafaker/NameTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 import static com.github.javafaker.matchers.MatchesRegularExpression.matchesRegularExpression;
 import static org.hamcrest.core.IsEqual.equalTo;
 import static org.hamcrest.core.IsNot.not;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.spy;
