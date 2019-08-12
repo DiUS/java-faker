ef6662b97f064b80d47cbf4cab4d7505054c36eb
Eric Goetschalckx <eric@goetschalckx.com>
2019-08-12 12:53:17 -0400

    add Nation.java and NationTest.java

--- src/main/java/com/github/javafaker/Nation.java
+++ src/main/java/com/github/javafaker/Nation.java
@@ -1,6 +1,13 @@
 package com.github.javafaker;
 
+import java.nio.ByteBuffer;
+import java.nio.MappedByteBuffer;
+import java.nio.charset.Charset;
+import java.util.List;
+
 public class Nation {
+
+    private static final Charset UTF8_CHARSET = Charset.forName("UTF-8");
 
     private final Faker faker;
 
@@ -18,6 +25,19 @@ public class Nation {
 
     public String capitalCity() {
         return faker.fakeValuesService().resolve("nation.capital_city", this, faker);
+    }
+
+    public String flag() {
+        @SuppressWarnings("unchecked")
+        List<Integer> flagInts = (List<Integer>) faker.fakeValuesService().fetch("nation.flag");
+
+        ByteBuffer byteBuffer = MappedByteBuffer.allocate(flagInts.size());
+
+        for (Integer flagInt : flagInts) {
+            byteBuffer.put(flagInt.byteValue());
+        }
+
+        return new String(byteBuffer.array(), UTF8_CHARSET);
     }
 
 }
