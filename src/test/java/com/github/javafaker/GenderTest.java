3c3a061b31e0aefc2a20dd423812779977f1d74d
Frank Mattes <frank.mattes@f-i-sp.de>
2020-02-05 14:55:43 +0100

    Tests for new class Gender
    - types
    - binaryTypes
    - shortBinaryTypes

---
+++ src/test/java/com/github/javafaker/GenderTest.java
@@ -0,0 +1,27 @@
+package com.github.javafaker;
+
+import static com.github.javafaker.matchers.MatchesRegularExpression.matchesRegularExpression;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+public class GenderTest extends AbstractFakerTest {
+
+    @Test
+    public void testType() {
+        assertThat(faker.gender().type(), matchesRegularExpression("\\w+"));
+    }
+
+    @Test
+    public void testBinaryType() {
+        final Gender gender = faker.gender();
+        assertThat(gender.binaryType(), matchesRegularExpression("Male|Female"));
+    }
+
+    @Test
+    public void testShortBinaryType() {
+        final Gender gender = faker.gender();
+        assertThat(gender.binaryType(), matchesRegularExpression("m|f"));
+    }
+
+}
