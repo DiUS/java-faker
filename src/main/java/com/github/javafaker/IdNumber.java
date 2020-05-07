bd8abd7466777b5da9f5248de7c4f688bbe22eb1
HcPlu <HcPlu@outlook.com>
2020-05-08 00:22:18 +0800

    fix 313

--- src/main/java/com/github/javafaker/IdNumber.java
+++ src/main/java/com/github/javafaker/IdNumber.java
@@ -2,6 +2,8 @@ package com.github.javafaker;
 
 import com.github.javafaker.idnumbers.EnIdNumber;
 import com.github.javafaker.idnumbers.SvSEIdNumber;
+import com.github.javafaker.idnumbers.ZhCnIdNumber;
+import org.yaml.snakeyaml.Yaml;
 
 public class IdNumber {
     private final Faker faker;
@@ -37,5 +39,14 @@ public class IdNumber {
     public String invalidSvSeSsn() {
         SvSEIdNumber svSEIdNumber = new SvSEIdNumber();
         return svSEIdNumber.getInvalidSsn(faker);
+    }
+
+    /**
+     * Specified as #{IDNumber.valid_sv_se_ssn} in zh-CN.yml
+     */
+
+    public String validZhCNSsn(){
+        ZhCnIdNumber zhCNIdNumber = new ZhCnIdNumber();
+        return zhCNIdNumber.getValidSsn(faker);
     }
 }
