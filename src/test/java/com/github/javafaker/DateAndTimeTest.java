bbb453920bd6ef145dcedf0ccfa6f641b5576685
irakatz <40564069+irakatz@users.noreply.github.com>
2020-05-27 12:38:56 +0800

    Update DateAndTimeTest.java

--- src/test/java/com/github/javafaker/DateAndTimeTest.java
+++ src/test/java/com/github/javafaker/DateAndTimeTest.java
@@ -8,8 +8,8 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.concurrent.TimeUnit;
-
 import static org.hamcrest.Matchers.*;
+
 import static org.junit.Assert.*;
 
 /**
