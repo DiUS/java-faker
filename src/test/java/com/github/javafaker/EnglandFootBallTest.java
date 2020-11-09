23c1eb238fbcdecd57361c39d02f19439fce0eea
scs9802 <uni_vers_@naver.com>
2020-11-09 21:48:26 +0900

    remove regular expression

--- src/test/java/com/github/javafaker/EnglandFootBallTest.java
+++ src/test/java/com/github/javafaker/EnglandFootBallTest.java
@@ -1,29 +1,27 @@
 package com.github.javafaker;
 
 
-
-
 import org.junit.Test;
 
-import static com.github.javafaker.matchers.MatchesRegularExpression.matchesRegularExpression;
+
 import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.assertThat;
 
 public class EnglandFootBallTest extends AbstractFakerTest{
 
-    private final String noLeadingTrailingWhitespaceRegex = "[A-Za-z ]+";
+
 
     @Test
     public void testLeague() {
         String league = faker.englandfootball().league();
         assertThat(league, not(isEmptyOrNullString()));
-        assertThat(league, matchesRegularExpression(noLeadingTrailingWhitespaceRegex));
+
     }
 
     @Test
     public void testTeam() {
         String team = faker.englandfootball().team();
         assertThat(team, not(isEmptyOrNullString()));
-        assertThat(team, matchesRegularExpression(noLeadingTrailingWhitespaceRegex));
+
     }
 }
