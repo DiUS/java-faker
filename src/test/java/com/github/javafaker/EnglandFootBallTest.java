ac8203fe513456c412b1f4aecfa20beed03dc66d
scs9802 <uni_vers_@naver.com>
2020-11-09 17:29:59 +0900

    fixed matchesRegularExpression.

--- src/test/java/com/github/javafaker/EnglandFootBallTest.java
+++ src/test/java/com/github/javafaker/EnglandFootBallTest.java
@@ -2,7 +2,7 @@ package com.github.javafaker;
 
 
 
-import com.github.javafaker.matchers.MatchesRegularExpression;
+
 import org.junit.Test;
 
 import static com.github.javafaker.matchers.MatchesRegularExpression.matchesRegularExpression;
@@ -11,19 +11,19 @@ import static org.junit.Assert.assertThat;
 
 public class EnglandFootBallTest extends AbstractFakerTest{
 
-    private final String noLeadingTrailingWhitespaceRegex = "^(?! )[A-Za-z0-9 ]*(?<! )$";
+    private final String noLeadingTrailingWhitespaceRegex = "[A-Za-z ]+";
 
     @Test
     public void testLeague() {
         String league = faker.englandfootball().league();
         assertThat(league, not(isEmptyOrNullString()));
-        assertThat(league, MatchesRegularExpression.matchesRegularExpression(noLeadingTrailingWhitespaceRegex));
+        assertThat(league, matchesRegularExpression(noLeadingTrailingWhitespaceRegex));
     }
 
     @Test
     public void testTeam() {
         String team = faker.englandfootball().team();
         assertThat(team, not(isEmptyOrNullString()));
-        assertThat(team, MatchesRegularExpression.matchesRegularExpression(noLeadingTrailingWhitespaceRegex));
+        assertThat(team, matchesRegularExpression(noLeadingTrailingWhitespaceRegex));
     }
 }
