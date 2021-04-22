97d048786e7fff45fdb598e0c3e87344e3bd7f2c
Jiapeng-Pei <11810127@mail.sustc.edu.cn>
2021-04-22 16:06:41 +0800

    Revert "Fix issue 611: StarCraftTest unit tests correction."
    This reverts commit af87bef6701cf20f7fef5daa655a9c925070e3d7.

--- src/test/java/com/github/javafaker/StarCraftTest.java
+++ src/test/java/com/github/javafaker/StarCraftTest.java
@@ -1,6 +1,5 @@
 package com.github.javafaker;
 
-import com.github.javafaker.repeating.Repeat;
 import org.junit.Test;
 
 import static com.github.javafaker.matchers.MatchesRegularExpression.matchesRegularExpression;
@@ -8,20 +7,12 @@ import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.assertThat;
 
 public class StarCraftTest extends AbstractFakerTest {
-    private final String noLeadingTrailingWhitespaceRegex = "^(?! )[-A-Za-z0-9' ]*(?<! )$";
+
+    private final String noLeadingTrailingWhitespaceRegex = "^(?! )[A-Za-z0-9' ]*(?<! )$";
 
     @Test
     public void testUnit() {
         String unit = faker.starCraft().unit();
-        assertThat(unit, not(isEmptyOrNullString()));
-        assertThat(unit, matchesRegularExpression(noLeadingTrailingWhitespaceRegex));
-    }
-
-    @Test
-    @Repeat(times = 1000)
-    public void testUnitOneThousand() {
-        String unit = faker.starCraft().unit();
-        // System.out.println(unit);
         assertThat(unit, not(isEmptyOrNullString()));
         assertThat(unit, matchesRegularExpression(noLeadingTrailingWhitespaceRegex));
     }
@@ -43,15 +34,6 @@ public class StarCraftTest extends AbstractFakerTest {
     @Test
     public void testPlanet() {
         String planet = faker.starCraft().planet();
-        assertThat(planet, not(isEmptyOrNullString()));
-        assertThat(planet, matchesRegularExpression(noLeadingTrailingWhitespaceRegex));
-    }
-
-    @Test
-    @Repeat(times = 1000)
-    public void testPlanetOneThousand() {
-        String planet = faker.starCraft().planet();
-        // System.out.println(planet);
         assertThat(planet, not(isEmptyOrNullString()));
         assertThat(planet, matchesRegularExpression(noLeadingTrailingWhitespaceRegex));
     }
