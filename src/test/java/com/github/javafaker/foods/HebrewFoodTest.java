6682ec0e9e83610822a1e613d0c01157ec43f93b
BravoChaoS <43561142+BravoChaoS@users.noreply.github.com>
2021-04-24 12:31:41 +0800

    Fix issue 598

---
+++ src/test/java/com/github/javafaker/foods/HebrewFoodTest.java
@@ -0,0 +1,50 @@
+package com.github.javafaker.foods;
+
+import com.github.javafaker.Faker;
+import com.github.javafaker.idnumbers.SvSEIdNumber;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Locale;
+
+import static com.github.javafaker.matchers.MatchesRegularExpression.matchesRegularExpression;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public class HebrewFoodTest {
+    // Test for issue 598
+
+    public final String matchHebrewFood = "[\\u0590-\\u05FF ']+";
+    public static Faker food;
+
+    @Before
+    public void before(){
+        food = new Faker(new Locale("he"));
+    }
+
+    @Test
+    public void hebrewIngredient() {
+        Assert.assertThat(food.food().ingredient(), matchesRegularExpression(matchHebrewFood));
+    }
+
+    @Test
+    public void hebrewFruit() {
+        Assert.assertThat(food.food().fruit(), matchesRegularExpression(matchHebrewFood));
+    }
+
+    @Test
+    public void hebrewVegetable() {
+        Assert.assertThat(food.food().vegetable(), matchesRegularExpression(matchHebrewFood));
+    }
+
+    @Test
+    public void hebrewSpice() {
+        Assert.assertThat(food.food().spice(), matchesRegularExpression(matchHebrewFood));
+    }
+
+    @Test
+    public void hebrewSushi() {
+        Assert.assertThat(food.food().sushi(), matchesRegularExpression(matchHebrewFood));
+    }
+}
