c50816fc43f35216930f23c5d580f47305a1e4ec
nanfang_wuyu <11811512@mail.sustech.edu.cn>
2021-04-25 21:58:23 +0800

    Update ChineseIdNumberTest.java
    Add new tests for public methods.

--- src/test/java/com/github/javafaker/idnumbers/ChineseIdNumberTest.java
+++ src/test/java/com/github/javafaker/idnumbers/ChineseIdNumberTest.java
@@ -9,9 +9,11 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
 
+//CS304 Issue link: https://github.com/DiUS/java-faker/issues/588
 public class ChineseIdNumberTest {
 
     @Test
@@ -56,6 +58,96 @@ public class ChineseIdNumberTest {
 
     }
 
+    @Test
+    public void zhCnSsnValidTest1() {
+        Faker f = new Faker(new Locale("zh-CN"));
+        ZhCNIdNumber zhCNIdNumber = new ZhCNIdNumber();
+
+        String idNo = f.idNumber().valid();
+        assertFalse(zhCNIdNumber.invalidCheck(f, idNo));
+    }
+
+    @Test
+    public void zhCnSsnValidTest2() {
+        Faker f = new Faker(new Locale("zh-CN"));
+        ZhCNIdNumber zhCNIdNumber = new ZhCNIdNumber();
+
+        String idNo = f.idNumber().valid();
+        assertEquals(idNo.length(), 18);
+    }
+
+    @Test
+    public void zhCnSsnInValidTest1() {
+        Faker f = new Faker(new Locale("zh-CN"));
+        ZhCNIdNumber zhCNIdNumber = new ZhCNIdNumber();
+
+        String inIdNo = f.idNumber().invalid();
+        assertThat(zhCNIdNumber.invalidCheck(f, inIdNo), is(true));
+    }
+
+    @Test
+    public void zhCnSsnInValidTest2() {
+        Faker f = new Faker(new Locale("zh-CN"));
+        ZhCNIdNumber zhCNIdNumber = new ZhCNIdNumber();
+
+        String inIdNo = f.idNumber().invalid();
+        assertTrue(zhCNIdNumber.invalidCheck(f, inIdNo));
+    }
+
+    @Test
+    public void getValidCNTest1() {
+        Faker f = new Faker(new Locale("zh-CN"));
+        ZhCNIdNumber zhCNIdNumber = new ZhCNIdNumber();
+
+        String idNo = zhCNIdNumber.getValidCN(f);
+        assertFalse(zhCNIdNumber.invalidCheck(f, idNo));
+    }
+
+    @Test
+    public void getValidCNTest2() {
+        Faker f = new Faker(new Locale("zh-CN"));
+        ZhCNIdNumber zhCNIdNumber = new ZhCNIdNumber();
+
+        String idNo = zhCNIdNumber.getValidCN(f);
+        assertEquals(idNo.length(), 18);
+    }
+
+    @Test
+    public void getInvalidCNTest1() {
+        Faker f = new Faker(new Locale("zh-CN"));
+        ZhCNIdNumber zhCNIdNumber = new ZhCNIdNumber();
+
+        String inIdNo = zhCNIdNumber.getInvalidCN(f);
+        assertThat(zhCNIdNumber.invalidCheck(f, inIdNo), is(true));
+    }
+
+    @Test
+    public void getInvalidCNTest2() {
+        Faker f = new Faker(new Locale("zh-CN"));
+        ZhCNIdNumber zhCNIdNumber = new ZhCNIdNumber();
+
+        String inIdNo = zhCNIdNumber.getInvalidCN(f);
+        assertTrue(zhCNIdNumber.invalidCheck(f, inIdNo));
+    }
+
+    @Test
+    public void invalidCheckTest1() {
+
+        Faker f = new Faker(new Locale("zh-CN"));
+        ZhCNIdNumber zhCNIdNumber = new ZhCNIdNumber();
+
+        String invalidId = "123456123456123456";
+        assertTrue(zhCNIdNumber.invalidCheck(f,invalidId));
+    }
+
+    @Test
+    public void invalidCheckTest2() {
+        Faker f = new Faker(new Locale("zh-CN"));
+        ZhCNIdNumber zhCNIdNumber = new ZhCNIdNumber();
+
+        String invalidId = "522622201303301923";
+        assertFalse(zhCNIdNumber.invalidCheck(f,invalidId));
+    }
 
 
 }
