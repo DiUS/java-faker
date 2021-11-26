db394b4cb02332532d8bb12c118433b952cf8103
Tyler Davis <tallyndavis@comcast.net>
2021-11-26 15:32:07 -0600

    initial test skeleton for FakeDuration

---
+++ src/test/java/com/github/javafaker/DurationTest.java
@@ -0,0 +1,22 @@
+package com.github.javafaker;
+
+import org.junit.Test;
+import java.time.Duration;
+
+import static org.junit.Assert.assertThat;
+
+public class DurationTest extends AbstractFakerTest {
+
+    @Test
+    public void testDurationObject(){
+        // int numMins = 60;
+        // Duration randomDuration = faker.duration().atMost(Duration.ofMinutes(numMins));
+        return;
+    }
+
+    @Test
+    public void testDurationString(){
+        return;
+    }
+
+}
