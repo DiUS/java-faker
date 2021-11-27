b7ea2689038916fa71d7e264b0ad8cfa534e9c30
Tyler Davis <tallyndavis@comcast.net>
2021-11-26 23:37:59 -0500

    Initial implementation + tests for FakeDuration

--- src/main/java/com/github/javafaker/FakeDuration.java
+++ src/main/java/com/github/javafaker/FakeDuration.java
@@ -9,7 +9,59 @@ public class FakeDuration {
         this.faker = faker;
     }
 
-    public Duration atMost(Duration minutes){
-        return Duration.ofMinutes(30);
+    /**
+    * Returns a random long between 0 and max inclusive
+    * 
+    * @param max: upper bound of number range
+    * 
+    * @return a random long between 0 and max inclusive
+    */
+    private long getNumberBetween(long max){
+        final long min = 0;
+        return this.faker.number().numberBetween(min, max + 1);  // numberBetween max is exclusive
+    }
+
+    /**
+    * Returns a Duration object representing a random duration of minutes in the given range
+    * 
+    * @param minutesMax: max number of minutes for Duration (inclusive)
+    * 
+    * @return a Duration object representing a number of minutes in the given range
+    */
+    public Duration atMostMinutes(long minutesMax){
+        return Duration.ofMinutes(getNumberBetween(minutesMax));
+    }
+
+    /**
+    * Returns a Duration object representing a random duration of hours in the given range
+    * 
+    * @param hoursMax: max number of hours for Duration (inclusive)
+    * 
+    * @return a Duration object representing a number of hours in the given range
+    */
+    public Duration atMostHours(long hoursMax){
+        return Duration.ofHours(getNumberBetween(hoursMax));
+    }
+
+    /**
+    * Returns a Duration object representing a random duration of days in the given range
+    * 
+    * @param daysMax: max number of days for Duration (inclusive)
+    * 
+    * @return a Duration object representing a number of days in the given range
+    */
+    public Duration atMostDays(long daysMax){
+        return Duration.ofDays(getNumberBetween(daysMax));
+    }
+
+    /**
+    * Returns a Duration object representing a random duration of seconds in the given range
+    * 
+    * @param secondsMax: max number of seconds for Duration (inclusive)
+    * 
+    * @return a Duration object representing a number of seconds in the given range
+    */
+    public Duration atMostSeconds(long secondsMax){
+        return Duration.ofSeconds(getNumberBetween(secondsMax));
     }
 }
