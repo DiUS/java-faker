5b41f8ae4081cbe1e6caf2f02ecc0ed8d566b234
Leon Cho <676442+leoncho@users.noreply.github.com>
2021-11-30 16:25:30 -0600

    refactor LocalePickerExample.java

--- src/main/java/com/github/javafaker/service/LocalePicker.java
+++ src/main/java/com/github/javafaker/service/LocalePicker.java
@@ -46,12 +46,4 @@ public class LocalePicker {
         return getRandomLocale(random);
     }
 
-    public static void main(String args[]) {
-
-        LocalePicker lp = new LocalePicker();
-        List<String> allLocales = lp.getAllSupportedLocales();
-        System.out.println("All Supported Locales: " + Arrays.toString(allLocales.toArray()));
-        String randomLocale = lp.getLocale();
-        System.out.println("Random Locale: " + randomLocale);
-    }
 }
--- src/test/java/com/github/javafaker/LocalePickerExample.java
+++ src/test/java/com/github/javafaker/LocalePickerExample.java
@@ -1,8 +1,11 @@
 package com.github.javafaker;
 
+import com.github.javafaker.service.FakeValuesService;
+
 import com.github.javafaker.service.LocalePicker;
 import java.util.List;
 import java.util.Arrays;
+import java.util.Locale;
 
 public class LocalePickerExample {
 
@@ -15,8 +18,30 @@ public class LocalePickerExample {
         System.out.println("All Supported Locales: " + Arrays.toString(allLocales.toArray()));
 
         // Get a random locale
-        String randomLocale = lp.getLocale();
-        System.out.println("Random Locale: " + randomLocale);
+        String randomLocaleString = lp.getLocale();
+        System.out.println("Random Locale: " + randomLocaleString);
+
+        // Instantiate a Faker object with a randomized locale
+        Locale randomLocale = new Locale(randomLocaleString);
+        Faker faker = new Faker(randomLocale);
+        String firstName = faker.name().firstName();
+        String lastName = faker.name().lastName();
+        String streetAddress = faker.address().streetAddress();
+        String phoneNumber = faker.phoneNumber().phoneNumber();
+        String fullName = faker.name().fullName();
+
+        System.out.println ("Full Name" + fullName);
+        System.out.println ("First Name: " + firstName);
+        System.out.println ("Last Name: " + lastName);
+        System.out.println ("Street Address: " + streetAddress);
+        System.out.println ("Phone Number: " + phoneNumber);
+
+        // Tests
+        System.out.println (randomLocale.getLanguage());
+        System.out.println (randomLocale.getCountry());
+        System.out.println (randomLocale.toString());
+
+
     }
 
     
