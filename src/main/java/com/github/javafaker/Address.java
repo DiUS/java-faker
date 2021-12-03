a1cbf71f18d900c6205e5850b541d4e581354f31
Tyler Davis <tallyndavis@comcast.net>
2021-12-03 18:37:56 -0500

    added links to issue

--- src/main/java/com/github/javafaker/Address.java
+++ src/main/java/com/github/javafaker/Address.java
@@ -31,6 +31,7 @@ public class Address {
         return faker.numerify(faker.fakeValuesService().resolve("address.secondary_address", this,faker));
     }
 
+    // CS427 Issue link: https://github.com/DiUS/java-faker/issues/591
     /**
     * Returns a String representing a standard 5-digit zip code.
     * 
@@ -40,6 +41,7 @@ public class Address {
         return faker.bothify(faker.fakeValuesService().resolve("address.postcode", this,faker));
     }
 
+    // CS427 Issue link: https://github.com/DiUS/java-faker/issues/591
     /**
     * Returns a String representing a ZIP+4 code for greater geographic specificity.
     * This is a 9-digit zip code, but the String itself is 10 characters including the hyphen.
