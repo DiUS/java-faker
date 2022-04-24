74e28f4b0fa96150abdbafda7fe5a715ba2afdf8
cooljj <1009839598@qq.com>
2022-04-24 22:40:10 +0800

    Add two tests for each public method in battlefield1

--- src/test/java/com/github/javafaker/Battlefield1Test.java
+++ src/test/java/com/github/javafaker/Battlefield1Test.java
@@ -3,6 +3,8 @@ package com.github.javafaker;
 import org.junit.Test;
 
 import static com.github.javafaker.matchers.MatchesRegularExpression.matchesRegularExpression;
+import static org.hamcrest.Matchers.isEmptyOrNullString;
+import static org.hamcrest.Matchers.not;
 import static org.junit.Assert.assertThat;
 
 public class Battlefield1Test extends AbstractFakerTest {
@@ -21,8 +23,26 @@ public class Battlefield1Test extends AbstractFakerTest {
      * CS304 (manually written) Issue link: https://github.com/DiUS/java-faker/issues/711
      */
     @Test
+    public void classesNotNull() {
+        assertThat(faker.battlefield1().classes(), not(isEmptyOrNullString()));
+    }
+
+    /**
+     *
+     * CS304 (manually written) Issue link: https://github.com/DiUS/java-faker/issues/711
+     */
+    @Test
     public void weapon() {
-        assertThat(faker.battlefield1().weapon(), matchesRegularExpression("[A-Za-z0-9,\\.\\(\\)\\/ ]+"));
+        assertThat(faker.battlefield1().weapon(), matchesRegularExpression("[A-Za-z0-9, \\-\\.\\(\\)\\/ ]+"));
+    }
+
+    /**
+     *
+     * CS304 (manually written) Issue link: https://github.com/DiUS/java-faker/issues/711
+     */
+    @Test
+    public void weaponNotNull() {
+        assertThat(faker.battlefield1().weapon(), not(isEmptyOrNullString()));
     }
 
     /**
@@ -39,6 +59,15 @@ public class Battlefield1Test extends AbstractFakerTest {
      * CS304 (manually written) Issue link: https://github.com/DiUS/java-faker/issues/711
      */
     @Test
+    public void vehicleNotNull() {
+        assertThat(faker.battlefield1().vehicle(), not(isEmptyOrNullString()));
+    }
+
+    /**
+     *
+     * CS304 (manually written) Issue link: https://github.com/DiUS/java-faker/issues/711
+     */
+    @Test
     public void map() {
         assertThat(faker.battlefield1().map(), matchesRegularExpression("[A-Za-z0-9,\\-\\.\\(\\)\\' ]+"));
     }
@@ -48,7 +77,25 @@ public class Battlefield1Test extends AbstractFakerTest {
      * CS304 (manually written) Issue link: https://github.com/DiUS/java-faker/issues/711
      */
     @Test
+    public void mapNotNull() {
+        assertThat(faker.battlefield1().map(), not(isEmptyOrNullString()));
+    }
+
+    /**
+     *
+     * CS304 (manually written) Issue link: https://github.com/DiUS/java-faker/issues/711
+     */
+    @Test
     public void faction() {
         assertThat(faker.battlefield1().faction(), matchesRegularExpression("[A-Za-z,\\- ]+"));
+    }
+
+    /**
+     *
+     * CS304 (manually written) Issue link: https://github.com/DiUS/java-faker/issues/711
+     */
+    @Test
+    public void factionNotNull() {
+        assertThat(faker.battlefield1().faction(), not(isEmptyOrNullString()));
     }
 }
