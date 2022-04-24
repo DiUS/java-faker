e1dfd7cd4224c704daa388e30f75316943afdfbd
Uncle-Road <457894974@qq.com>
2022-04-24 23:19:21 +0800

    add check null test

--- src/test/java/com/github/javafaker/WeaponTest.java
+++ src/test/java/com/github/javafaker/WeaponTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 import static com.github.javafaker.matchers.MatchesRegularExpression.matchesRegularExpression;
 import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.*;
 
 public class WeaponTest extends AbstractFakerTest {
 
@@ -27,6 +28,15 @@ public class WeaponTest extends AbstractFakerTest {
      * CS304 issue link:https://github.com/DiUS/java-faker/issues/715
      */
     @Test
+    public void provenanceNotNullTest(){
+        assertThat(faker.weapon().provenance(),not(isEmptyOrNullString()));
+    }
+
+
+    /**
+     * CS304 issue link:https://github.com/DiUS/java-faker/issues/715
+     */
+    @Test
     public void userTest(){
         assertThat(faker.weapon().user(), matchesRegularExpression("[a-zA-Z0-9\\-\\.\\ \\/]+"));
     }
@@ -35,8 +45,25 @@ public class WeaponTest extends AbstractFakerTest {
      * CS304 issue link:https://github.com/DiUS/java-faker/issues/715
      */
     @Test
+    public void userNotNullTest(){
+        assertThat(faker.weapon().user(),not(isEmptyOrNullString()));
+    }
+
+
+    /**
+     * CS304 issue link:https://github.com/DiUS/java-faker/issues/715
+     */
+    @Test
     public void functionTest(){
         assertThat(faker.weapon().function(), matchesRegularExpression("[a-zA-Z0-9\\-\\.\\ \\/]+"));
+    }
+
+    /**
+     * CS304 issue link:https://github.com/DiUS/java-faker/issues/715
+     */
+    @Test
+    public void functionNotNullTest(){
+        assertThat(faker.weapon().function(),not(isEmptyOrNullString()));
     }
 
     /**
@@ -51,8 +78,24 @@ public class WeaponTest extends AbstractFakerTest {
      * CS304 issue link:https://github.com/DiUS/java-faker/issues/715
      */
     @Test
+    public void targetNotNullTest(){
+        assertThat(faker.weapon().target(),not(isEmptyOrNullString()));
+    }
+
+    /**
+     * CS304 issue link:https://github.com/DiUS/java-faker/issues/715
+     */
+    @Test
     public void typeTest(){
         assertThat(faker.weapon().type(), matchesRegularExpression("[a-zA-Z0-9\\-\\.\\ \\/]+"));
+    }
+
+    /**
+     * CS304 issue link:https://github.com/DiUS/java-faker/issues/715
+     */
+    @Test
+    public void typeNotNullTest(){
+        assertThat(faker.weapon().type(),not(isEmptyOrNullString()));
     }
 
 }
