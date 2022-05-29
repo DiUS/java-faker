2a38c2a001413e42225dcc669d12622a60e64688
cooljj <1009839598@qq.com>
2022-05-29 21:45:53 +0800

    Bug update

--- src/main/java/com/github/javafaker/SwiftCode.java
+++ src/main/java/com/github/javafaker/SwiftCode.java
@@ -1,5 +1,7 @@
 package com.github.javafaker;
 
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+
 /**
  * CS304 Issue link: https://github.com/DiUS/java-faker/issues/721
  * A class for generating random swift code
@@ -7,56 +9,58 @@ package com.github.javafaker;
 
 public class SwiftCode {
 
-    private final Faker faker;
+  private final Faker faker;
 
-    protected SwiftCode(final Faker faker) {
-        this.faker = faker;
-    }
+  protected SwiftCode(final Faker faker) {
+    this.faker = faker;
+  }
 
-    /**
-     *
-     * @return a random swift code of a bank's headquarter as a string value
-     */
-    public String headquarterSwiftcode() {
-        String chars1 = "ABCDEFGHIZKLMNOPQRSTUVWXYZ";
-        String chars2 = "0123456789ABCDEFGHIZKLMNOPQRSTUVWXYZ";
-        String swiftcode = "";
-        for (int i = 0 ; i < 4 ; i++) {
-            String bank = String.valueOf(chars1.charAt((int) (Math.random() * 26)));
-            swiftcode += bank;
-        }
-        String country = faker.address().countryCode();
-        swiftcode += country;
-        for (int i = 0 ; i < 2 ; i++) {
-            String region = String.valueOf(chars2.charAt((int) (Math.random() * 36)));
-            swiftcode += region;
-        }
-        swiftcode += "XXX";
-        return swiftcode;
+  /**
+  * The
+  * @return a random swift code of a bank's headquarter as a string value
+  */
+  @SuppressFBWarnings({"DM_NEXTINT_VIA_NEXTDOUBLE", "PREDICTABLE_RANDOM"})
+  public String headquarterSwiftcode() {
+    String chars1 = "ABCDEFGHIZKLMNOPQRSTUVWXYZ";
+    String chars2 = "0123456789ABCDEFGHIZKLMNOPQRSTUVWXYZ";
+    StringBuffer swiftcode = new StringBuffer();
+    for (int i = 0; i < 4; i++) {
+      String bank = String.valueOf(chars1.charAt((int) (Math.random() * 26)));
+      swiftcode.append(bank);
     }
+    String country = faker.address().countryCode();
+    swiftcode.append(country);
+    for (int i = 0; i < 2; i++) {
+      String region = String.valueOf(chars2.charAt((int) (Math.random() * 36)));
+      swiftcode.append(region);
+    }
+    swiftcode.append("XXX");
+    return swiftcode.toString();
+  }
 
-    /**
-     *
-     * @return a random swift code of a bank's branch as a string value
-     */
-    public String branchSwiftcode() {
-        String chars1 = "ABCDEFGHIZKLMNOPQRSTUVWXYZ";
-        String chars2 = "0123456789ABCDEFGHIZKLMNOPQRSTUVWXYZ";
-        String swiftcode = "";
-        for (int i = 0 ; i < 4 ; i++) {
-            String bank = String.valueOf(chars1.charAt((int) (Math.random() * 26)));
-            swiftcode += bank;
-        }
-        String country = faker.address().countryCode();
-        swiftcode += country;
-        for (int i = 0 ; i < 2 ; i++) {
-            String region = String.valueOf(chars2.charAt((int) (Math.random() * 36)));
-            swiftcode += region;
-        }
-        for (int i = 0 ; i < 3 ; i++) {
-            String branch = String.valueOf(chars2.charAt((int) (Math.random() * 36)));
-            swiftcode += branch;
-        }
-        return swiftcode;
+  /**
+   *
+   * @return a random swift code of a bank's branch as a string value
+   */
+  @SuppressFBWarnings({"PREDICTABLE_RANDOM", "DM_NEXTINT_VIA_NEXTDOUBLE"})
+  public String branchSwiftcode() {
+    String chars1 = "ABCDEFGHIZKLMNOPQRSTUVWXYZ";
+    String chars2 = "0123456789ABCDEFGHIZKLMNOPQRSTUVWXYZ";
+    StringBuffer swiftcode = new StringBuffer();
+    for (int i = 0; i < 4; i++) {
+      String bank = String.valueOf(chars1.charAt((int) (Math.random() * 26)));
+      swiftcode.append(bank);
     }
+    String country = faker.address().countryCode();
+    swiftcode.append(country);
+    for (int i = 0; i < 2; i++) {
+      String region = String.valueOf(chars2.charAt((int) (Math.random() * 36)));
+      swiftcode.append(region);
+    }
+    for (int i = 0; i < 3; i++) {
+      String branch = String.valueOf(chars2.charAt((int) (Math.random() * 36)));
+      swiftcode.append(branch);
+    }
+    return swiftcode.toString();
+  }
 }
